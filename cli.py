fc0e493a75ea0a7a4eaf48b911f1f676ebc4999c
Mike Spiegel <michael.spiegel@tdameritrade.com>
2016-07-27 16:48:28 -0700

    updated function for create_profile

--- cli.py
+++ cli.py
@@ -1,11 +1,75 @@
 import jira
 import click
 import json
+import base64
+import os.path as path
+import csv
+import warnings
 
-@click.command()
-def login():
-    click.echo("logging you in")
+warnings.filterwarnings('ignore')
 
+HOME_DIRECTORY = path.expanduser('~')
+CONFIG_FILE = path.join(HOME_DIRECTORY,'.jira_getter.config.json')
+OPTIONS = {
+    'server': 'https://jira-ct.associatesys.local/',
+    'verify': False
+}
+
+
+def has_config_file():
+    pass
+
+
+def get_config(config):
+    with open(config, 'r') as f:
+        data = json.load(f)
+
+    return data
+
+def parse_config(config):
+    return (config['username'], base64.b64decode(config['password']))
+
+@click.group()
+def cli():
+    """A global namespace for JIRA commands"""
+    pass
+
+@cli.command()
+@click.option('--username', prompt=True)
+@click.password_option()
+def create_profile(username, password):
+    """encodes and serializes JIRA login credentials
+
+    passwords are encoded and stored on your home directory
+    """
+
+    encoded_password = base64.b64encode(password)
+
+    payload = {
+    'username': username,
+    'password': encoded_password
+    }
+
+    with open(CONFIG_FILE, 'w') as f:
+        json.dump(payload, f)
+        click.echo("success!  config file written to: {}".format(CONFIG_FILE))
+
+
+
+
+@cli.command()
+def download_all_data():
+    """downloads all data from JIRA
+
+    WARNING:  this operation will take several minutes
+    """
+
+    auth_tup = parse_config(get_config(CONFIG_FILE))
+    jac = jira.JIRA(OPTIONS, basic_auth=auth_tup)
+
+
+
+    click.echo("downloading")
 
 if __name__ == '__main__':
-    login()
+    cli()
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,9 +1,13 @@
 import click
 from click.testing import CliRunner
 import cli
+import os.path as path
 
-def test_login():
-    runner = CliRunner()
-    result = runner.invoke(cli.login)
-    assert result.exit_code == 0
-    assert result.output == 'logging you in\n'
+HOME_DIRECTORY = path.expanduser('~')
+CONFIG_FILE = path.join(HOME_DIRECTORY,'.jira_getter.config.json')
+
+def test_create_profile():
+    pass
+
+def test_download_all_data():
+    pass
