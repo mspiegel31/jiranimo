eeddaa48bbc67a5f40cd71428432bd61514caccc
Mike Spiegel <michael.spiegel@tdameritrade.com>
2016-08-06 10:06:27 -0400

    added more specific tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -59,3 +59,23 @@ class TestDownloadAllData:
 
         assert mock_jira_instance.search_issues.called
         mock_jira_instance.search_issues.assert_called_with('project = AMDG AND issuetype in (Defect, "Developer Story", Epic) AND sprint in ("DEV")', fields=default_fields, maxResults=10)
+
+    @patch('cli.process_issues')
+    @patch('csv.DictWriter')
+    @patch('cli.parse_config')
+    @patch('cli.path.isfile')
+    @patch('cli.jira.JIRA')
+    def test_it_should_request_a_default_set_of_fields(self, MockJira, mock_is_file, mock_parse_config, MockDictWriter, mock_process_issues):
+        mock_is_file.return_value = True
+        mock_parse_config.return_value = ('foo', 'bar')
+        mock_jira_instance = MockJira.return_value
+        default_fields = ['customfield_10406', 'status', 'customfield_10143', 'resolutiondate']
+        additional_fields = ['1','2','3']
+
+        expected_fields = ','.join(default_fields + additional_fields)
+
+        runner = CliRunner()
+        result = runner.invoke(cli.download_all_data, ['foo.csv'] + additional_fields)
+
+        assert mock_jira_instance.search_issues.called
+        mock_jira_instance.search_issues.assert_called_with('project = AMDG AND issuetype in (Defect, "Developer Story", Epic) AND sprint in ("DEV")', fields=expected_fields, maxResults=10)
