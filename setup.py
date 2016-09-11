4e946458dc3ee38e3a776b92af38e3cb330e9c83
Mike Spiegel <michael.spiegel@tdameritrade.com>
2016-09-11 11:15:51 -0400

    draft setup.py file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(name='jiranimo',
+      version='0.1',
+      description='utils for getting data out of Jira',
+      author='Mike Spiegel',
+      author_email='mspiegel31@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'Click',
+          'jira',
+      ],
+
+      )
